ec2f97ed0068e79ccc1b5627988758b93d4c5f47
kaustubh_choudhary <kaustubhchoudhary@yahoo.com>
2024-12-09 03:17:13 +0530

    Added student class

--- Main.java
+++ Main.java
@@ -3,7 +3,15 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("\n Testing jenkins with git");
 		System.out.println("\n Testing 1 minute executions");
+		
+		Student.saysHello();
 	}
+}
 
-
+class Student {
+	
+	public static void saysHello (){
+		System.out.println("\n Student says hello");
+	}
+	
 }
