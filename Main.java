93f59f7f97022d2f1bbab63067a2552114b7b7e9
kaustubh_choudhary <kaustubhchoudhary@yahoo.com>
2024-12-09 03:11:45 +0530

    Added a line

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
 	
 	public static void main(String[] args){
 		System.out.println("\n Testing jenkins with git");
+		System.out.println("\n Testing 1 minute executions");
 	}
 
 
